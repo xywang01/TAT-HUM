implementation: fixed the spatial transformation issue by normalizing all normal vectors, inverting the vector if pointing downwards, subtracting the angle of rotation to ground from 90

demo/sample_data_analysis.py
@@ -33,8 +33,8 @@ trajectory_path = '~/Downloads/data/trajectory_data'  # individual trajectory fi
 
 # booleans to determine whether to visualize 3D calibration results. Setting up a boolean controller at the beginning
 # of the analysis script allows one to easily toggle different functionalities of the analysis using comment/uncomment
-plot_3d_calibration_check = True
-# plot_3d_calibration_check = False
+# plot_3d_calibration_check = True
+plot_3d_calibration_check = False
 
 # max number of missing trials before going to visual inspection
 n_missing_max = 15
@@ -49,8 +49,8 @@ target_location_2d = (
     [140., 0., -np.sqrt(220. ** 2 - 140. ** 2)],)
 
 # a list of participants id's
-# par_id_all = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
-par_id_all = (2, )
+par_id_all = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+# par_id_all = (3, )
 
 # % process screen calibration data using custom function
 
@@ -271,9 +271,9 @@ for par_id_ind, par_id in enumerate(par_id_all):
                         # ax_3d.plot(raw_data.x - screen_center[0],
                         #            raw_data.y - screen_center[1],
                         #            raw_data.z - screen_center[2])
-                        # ax_3d.scatter(end_points[:, 0],
-                        #               end_points[:, 1],
-                        #               end_points[:, 2])
+                        ax_3d.scatter(end_points[:, 0],
+                                      end_points[:, 1],
+                                      end_points[:, 2], marker='x', color='black')
                         ax_3d.set_xlabel('x')
                         ax_3d.set_ylabel('y')
                         ax_3d.set_zlabel('z')
@@ -355,7 +355,7 @@ output_df['rt'] = output_df['rt'] * 1000
 output_df['mt'] = output_df['mt'] * 1000
 
 output_df['keep_both'] = output_df['keep_trial'] & output_df['keep_trajectory']
-# output_df.to_csv(output_save_name)
+output_df.to_csv(output_save_name)
 
 # %% analyze trajectory congruency area
 

tathum/functions.py
@@ -290,15 +290,18 @@ def compute_transformation_3d(surface_points_x: np.ndarray,
     # find the best fitting plane and its surface normal
     surface_plane = Plane.best_fit(surface_points)
     surface_norm = Vector(surface_plane.cartesian()[:3])
+    surface_norm = surface_norm / np.linalg.norm(surface_norm)
+
+    if surface_norm[horizontal_ind] < 0:
+        surface_norm *= -1
 
     # project the current surface normal to the horizontal plane
     screen_norm_ground = surface_norm - horizontal_norm.project_vector(surface_norm)
+    screen_norm_ground = screen_norm_ground / np.linalg.norm(screen_norm_ground)
 
     # find the angle between the projected norm and the primary direction - this is to align the screen's primary
     # direction with the primary direction in the Cartesian coordinate
     angle = screen_norm_ground.angle_between(primary_norm)
-    # make sure the angle is less than 90°
-    angle = np.pi - angle if np.abs(angle) > np.pi / 2 else angle
 
     # construct the rotation matrix and rotate the surface normal
     rotmat = matrix_from_axis_angle(np.hstack((horizontal_norm, (angle,))))
@@ -308,6 +311,7 @@ def compute_transformation_3d(surface_points_x: np.ndarray,
     # after the screen is aligned with the primary axis, we can rotate it around the secondary axis to make the
     # screen surface horizontal
     angle = screen_norm_rot.angle_between(horizontal_norm)
+    angle = np.pi - angle
 
     # axis is around the secondary direction
     rotmat = matrix_from_axis_angle(np.hstack((secondary_norm, (angle,))))